6f8e1e59727e6aace3e85bbe7d10f8094c3b34ca
Shiv Bajpai <shiv.bajpai.5@gmail.com>
2022-02-23 19:07:00 +0530

    Ball pick Up and drop testting

--- Assets/Scripts/PickUpScript.cs
+++ Assets/Scripts/PickUpScript.cs
@@ -3,38 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace GameJam
 {
+    enum BallState
+    {
+        WithPlayer = 0,
+        AwayFromPlayer =1,
+
+    }
 public class PickUpScript : MonoBehaviour
 {
         [SerializeField] private string ballTag = "ball";
         [SerializeField] private float checkRadius = 1f;
         [SerializeField] private Vector3 ballCheckOffset = new Vector3(0,0,0);
+        [SerializeField] private float pickOffset = 0.8f;
+        [HideInInspector]
+        BallState ballState = BallState.AwayFromPlayer;
+        private GameObject PlayerObject;
 
-
-        private void OnTriggerStay(Collider other)
+        private void Start()
         {
-            //Debug.Log("are we event colliding here");
-            
+            PlayerObject = FindObjectOfType<PlayerInput>().gameObject;
         }
         private void Update()
         {
-            //Debug.Log(gameObject.tag);
+            //StickToPlayer();
             Collider[] cols = Physics.OverlapSphere(ballCheckOffset + new Vector3(transform.position.x, transform.position.y,transform.position.z), checkRadius);
             for (int i = 0; i < cols.Length; i++)
             {
                 if (cols[i].CompareTag("ball"))
                 {
-                    Debug.Log("we are near a ball");
+                    ballState = BallState.WithPlayer;
+                    Debug.Log("Ball State changed");
+                    return;
                 }
-                //if (cols[i].gameObject != gameObject && !cols[i].isTrigger && !cols[i].CompareTag("Portal"))
-                //{
-                //    if (!isGrounded) isGrounded = true;
-                //}
+                ballState = BallState.AwayFromPlayer;
             }
         }
         void OnDrawGizmos()
         {
             Gizmos.color = Color.yellow;
             Gizmos.DrawWireSphere(transform.position + new Vector3(ballCheckOffset.x, ballCheckOffset.y), checkRadius);
+        }
+        void StickToPlayer()
+        {
+            if(ballState == BallState.WithPlayer)
+            {
+                transform.position = new Vector3(PlayerObject.transform.position.x, PlayerObject.transform.position.y + 3, PlayerObject.transform.position.z);
+
+            }
         }
     }
 
